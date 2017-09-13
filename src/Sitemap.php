b147c2fa8d7aa92cd0717ab959a558312eae8db0
Rohit Agarwal <1993rohitagarwal@gmail.com>
2017-09-14 03:11:57 +0530

    file closing and buffer check implemented

--- src/Sitemap.php
+++ src/Sitemap.php
@@ -16,7 +16,10 @@ class Sitemap
     const FREQUENCY_YEARLY = 'yearly';
     const FREQUENCY_NEVER = 'never';
 
+    const ELEMENT_URLSET = 'urlset';
+
     protected $xmlWriter;
+    protected $urlsCount = 0;
     protected $buffer = 1000;
     protected $maxUrls = 2000;
 
@@ -27,11 +30,13 @@ class Sitemap
 
     protected function newFile()
     {
+        $this->urlsCount = 0;
         $this->xmlWriter = new XMLWriter;
         $this->xmlWriter->openMemory();
         $this->xmlWriter->setIndent(true);
         $this->xmlWriter->setIndentString(' ');
         $this->xmlWriter->startDocument('1.0', 'UTF-8');
+        $this->xmlWriter->startElement(self::ELEMENT_URLSET);
     }
 
     public function setBuffer($buffer)
@@ -46,9 +51,25 @@ class Sitemap
         return $this;
     }
 
+    protected function getFilePath()
+    {
+    }
+
+    protected function validateSize()
+    {
+        if ($this->urlsCount >= $this->maxUrls) {
+            $this->finish();
+            $this->newFile();
+        } elseif ($this->urlsCount % $this->buffer == 0) {
+            $this->flush();
+        }
+    }
+
     public function addUrl($loc, $lastmod = null, $priority = null, $freq = null)
     {
+        $this->validateSize();
         $url = new Url($this->xmlWriter, $loc);
+
         if ($lastmod instanceof Closure) {
             call_user_func($lastmod, $url);
         } elseif (!empty($lastmod)) {
@@ -63,6 +84,22 @@ class Sitemap
             $url->setFrequency($freq);
         }
 
+        $url->finish();
+        $this->urlsCount++;
+
         return $this;
+    }
+
+    protected function flush()
+    {
+        $filePath = $this->getFilePath();
+        file_put_contents($filePath, $this->xmlWriter->flush(true), FILE_APPEND);
+    }
+
+    public function finish()
+    {
+        $this->xmlWriter->endElement();
+        $this->xmlWriter->endDocument();
+        $this->flush();
     }
 }
--- src/Url.php
+++ src/Url.php
@@ -16,10 +16,11 @@ class Url
     public function __construct($xmlWriter, $loc)
     {
         $this->xmlWriter = $xmlWriter;
+        $this->xmlWriter->startElement(self::ELEMENT_URL);
         $this->setLoc($loc);
     }
 
-    private function setLoc($loc)
+    protected function setLoc($loc)
     {
         $this->xmlWriter->startElement(Self::ELEMENT_LOC);
         $this->xmlWriter->text($loc);
@@ -44,6 +45,11 @@ class Url
     {
         $this->xmlWriter->startElement(Self::ELEMENT_FREQUENCY);
         $this->xmlWriter->text($frequency);
+        $this->xmlWriter->endElement();
+    }
+
+    public function finish()
+    {
         $this->xmlWriter->endElement();
     }
 }
